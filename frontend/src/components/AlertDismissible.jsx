016de518d5b177caafc81d3bf7e3a9f5763c75fe
JB-Dev <jibaoke@gmail.com>
2022-09-16 13:57:47 +0200

    structure project Version 1

---
+++ frontend/src/components/AlertDismissible.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AlertDismissible() {
+  return (
+    <div className='AlertDismmissible'>AlertDismissible</div>
+  )
+}
---
+++ frontend/src/components/ProfileItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProfileItem() {
+  return (
+    <div className='ProfilItem'>ProfileItem</div>
+  )
+}
